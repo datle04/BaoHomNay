6d7ea39540d343e0213c80d3101cee854267856b
Dat Le <ldat0909@gmail.com>
2025-06-03 20:01:11 +0700

    .

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+app.get('/', (req, res) => {
+  res.send('Backend is running ✅');
+});
+
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log(`Server running on port ${process.env.PORT}`);
     });
   })
